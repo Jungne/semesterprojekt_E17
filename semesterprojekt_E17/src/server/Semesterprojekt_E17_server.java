427f0f9830cd50e8b168b7ff65c615622ddfc2fd
Jungne <jungne7@gmail.com>
2017-10-23 14:01:21 +0200

    Jeg ved ikke

--- semesterprojekt_E17/src/server/Semesterprojekt_E17_server.java
+++ semesterprojekt_E17/src/server/Semesterprojekt_E17_server.java
@@ -5,6 +5,10 @@
  */
 package server;
 
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author jungn
@@ -15,7 +19,11 @@ public class Semesterprojekt_E17_server {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-	// TODO code application logic here
+	try {
+	    ServerControllerImpl serverController = new ServerControllerImpl();
+	} catch (RemoteException ex) {
+	    Logger.getLogger(Semesterprojekt_E17_server.class.getName()).log(Level.SEVERE, null, ex);
+	}
     }
     
 }
