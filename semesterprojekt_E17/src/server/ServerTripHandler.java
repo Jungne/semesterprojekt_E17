da7ad64ceca813eb6452d82258c1d4bed05163d7
Hjalte Fromholt Rindom <hjaltefromholtrindom@Hjaltes-MacBook-Air.local>
2017-10-25 13:40:42 +0200

    deleteTrip impl

--- semesterprojekt_E17/src/server/ServerTripHandler.java
+++ semesterprojekt_E17/src/server/ServerTripHandler.java
@@ -4,9 +4,17 @@ import database.DBManager;
 import interfaces.Trip;
 
 public class ServerTripHandler {
+
     public static void createTrip(Trip newTrip) {
-	String query = "INSERT INTO Trips "; //TODO
-	
-	DBManager.getInstance().executeUpdate(query);
+        String query = "INSERT INTO Trips "; //TODO
+
+        DBManager.getInstance().executeUpdate(query);
+    }
+
+    public static void deleteTrip(Trip trip) {
+        String query = "DELETE FROM Trips WHERE tripID = " + trip.getID() + ";";
+
+        DBManager.getInstance().executeUpdate(query);
+
     }
 }
