51bd6912bfcd849aa066a2b9c87a5da2bcbfbc88
Jungne <jungne7@gmail.com>
2017-10-25 12:00:19 +0200

    Established client-server connection

--- semesterprojekt_E17/src/client/FXMLDocumentController.java
+++ semesterprojekt_E17/src/client/FXMLDocumentController.java
@@ -1,34 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package client;
 
 import java.net.URL;
+import java.rmi.RemoteException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-/**
- *
- * @author jungn
- */
 public class FXMLDocumentController implements Initializable {
-    
+
+    private ClientController clientController;
+
     @FXML
     private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-	System.out.println("You clicked me!");
-	label.setText("Hello World!");
-    }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    }    
-    
+	try {
+	    clientController = new ClientController();
+	} catch (RemoteException ex) {
+	    Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+	}
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+	
+	try {
+	    System.out.println(clientController.getAllTrips().get(0).getTitle());
+	} catch (RemoteException ex) {
+	    Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+	}
+    }
 }
