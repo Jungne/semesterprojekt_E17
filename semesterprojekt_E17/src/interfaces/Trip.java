2d29fceee999bd43d06595541e8c2b3a8c592da9
Jungne <jungne7@gmail.com>
2017-10-26 15:08:11 +0200

    Finished implementing searchTrips()
    Still needs testing.

--- semesterprojekt_E17/src/interfaces/Trip.java
+++ semesterprojekt_E17/src/interfaces/Trip.java
@@ -7,169 +7,169 @@ import javafx.scene.image.Image;
 
 public class Trip implements Serializable {
 
-    private int id;
-    private String title;
-    private String description;
-    private double price;
-    private User organizer;
-    private Date timeStart;
-    private String meetingAddress;
-    private Location location;
-    private int participantlimit;
-    private Conversation conversation;
-    private String category;
-    private List<User> participants;
-    private List<User> instructors;
-    private List<Category> categories;
-    private List<OptionalPrice> optionalPrices;
-    private List<Image> images;
-    private List<String> tags;
+	private int id;
+	private String title;
+	private String description;
+	private double price;
+	private User organizer;
+	private Date timeStart;
+	private String meetingAddress;
+	private Location location;
+	private int participantlimit;
+	private Conversation conversation;
+	private String category;
+	private List<User> participants;
+	private List<User> instructors;
+	private List<Category> categories;
+	private List<OptionalPrice> optionalPrices;
+	private Image image;
+	private List<String> tags;
 
-    public Trip(int id, String title) {
-	this.id = id;
-	this.title = title;
-    }
+	public Trip(int id, String title) {
+		this.id = id;
+		this.title = title;
+	}
 
-    public Trip(int id, String title, String description, double price, List<Image> images) {
-	this.id = id;
-	this.title = title;
-	this.description = description;
-	this.price = price;
-	this.images = images;
-    }
+	public Trip(int id, String title, String description, double price, Image image) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.price = price;
+		this.image = image;
+	}
 
-    public Trip(int id, String title, String description, double price, User organizer, String meetingAddress, Location location, int participantlimit, Conversation conversation, String category, List<User> participants, List<User> instructors, List<Category> categories, List<OptionalPrice> optionalPrices, List<String> tags) {
-	this.id = id;
-	this.title = title;
-	this.description = description;
-	this.price = price;
-	this.organizer = organizer;
-	this.meetingAddress = meetingAddress;
-	this.location = location;
-	this.participantlimit = participantlimit;
-	this.conversation = conversation;
-	this.category = category;
-	this.optionalPrices = optionalPrices;
-	this.participants = participants;
-	this.instructors = instructors;
-	this.categories = categories;
-	this.tags = tags;
-    }
+	public Trip(int id, String title, String description, double price, User organizer, String meetingAddress, Location location, int participantlimit, Conversation conversation, String category, List<User> participants, List<User> instructors, List<Category> categories, List<OptionalPrice> optionalPrices, List<String> tags) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.price = price;
+		this.organizer = organizer;
+		this.meetingAddress = meetingAddress;
+		this.location = location;
+		this.participantlimit = participantlimit;
+		this.conversation = conversation;
+		this.category = category;
+		this.optionalPrices = optionalPrices;
+		this.participants = participants;
+		this.instructors = instructors;
+		this.categories = categories;
+		this.tags = tags;
+	}
 
-    public void participate(User user) {
-	getParticipants().add(user);
-    }
+	public void participate(User user) {
+		getParticipants().add(user);
+	}
 
-    /**
-     * @return the id
-     */
-    public int getId() {
-	return id;
-    }
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
 
-    /**
-     * @return the title
-     */
-    public String getTitle() {
-	return title;
-    }
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
 
-    /**
-     * @return the description
-     */
-    public String getDescription() {
-	return description;
-    }
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
 
-    /**
-     * @return the price
-     */
-    public double getPrice() {
-	return price;
-    }
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
 
-    /**
-     * @return the organizer
-     */
-    public User getOrganizer() {
-	return organizer;
-    }
+	/**
+	 * @return the organizer
+	 */
+	public User getOrganizer() {
+		return organizer;
+	}
 
-    /**
-     * @return the timeStart
-     */
-    public Date getTimeStart() {
-	return timeStart;
-    }
+	/**
+	 * @return the timeStart
+	 */
+	public Date getTimeStart() {
+		return timeStart;
+	}
 
-    /**
-     * @return the meetingAddress
-     */
-    public String getMeetingAddress() {
-	return meetingAddress;
-    }
+	/**
+	 * @return the meetingAddress
+	 */
+	public String getMeetingAddress() {
+		return meetingAddress;
+	}
 
-    /**
-     * @return the location
-     */
-    public Location getLocation() {
-	return location;
-    }
+	/**
+	 * @return the location
+	 */
+	public Location getLocation() {
+		return location;
+	}
 
-    /**
-     * @return the participantlimit
-     */
-    public int getParticipantlimit() {
-	return participantlimit;
-    }
+	/**
+	 * @return the participantlimit
+	 */
+	public int getParticipantlimit() {
+		return participantlimit;
+	}
 
-    /**
-     * @return the conversation
-     */
-    public Conversation getConversation() {
-	return conversation;
-    }
+	/**
+	 * @return the conversation
+	 */
+	public Conversation getConversation() {
+		return conversation;
+	}
 
-    /**
-     * @return the category
-     */
-    public String getCategory() {
-	return category;
-    }
+	/**
+	 * @return the category
+	 */
+	public String getCategory() {
+		return category;
+	}
 
-    /**
-     * @return the prices
-     */
-    public List<OptionalPrice> getOptionalPrices() {
-	return optionalPrices;
-    }
+	/**
+	 * @return the prices
+	 */
+	public List<OptionalPrice> getOptionalPrices() {
+		return optionalPrices;
+	}
 
-    /**
-     * @return the participants
-     */
-    public List<User> getParticipants() {
-	return participants;
-    }
+	/**
+	 * @return the participants
+	 */
+	public List<User> getParticipants() {
+		return participants;
+	}
 
-    /**
-     * @return the instructors
-     */
-    public List<User> getInstructors() {
-	return instructors;
-    }
+	/**
+	 * @return the instructors
+	 */
+	public List<User> getInstructors() {
+		return instructors;
+	}
 
-    /**
-     * @return the categories
-     */
-    public List<Category> getCategories() {
-	return categories;
-    }
+	/**
+	 * @return the categories
+	 */
+	public List<Category> getCategories() {
+		return categories;
+	}
 
-    /**
-     * @return the tags
-     */
-    public List<String> getTags() {
-	return tags;
-    }
+	/**
+	 * @return the tags
+	 */
+	public List<String> getTags() {
+		return tags;
+	}
 
 }
--- semesterprojekt_E17/src/server/ServerTripHandler.java
+++ semesterprojekt_E17/src/server/ServerTripHandler.java
@@ -2,6 +2,8 @@ package server;
 
 import database.DBManager;
 import interfaces.Trip;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -9,68 +11,82 @@ import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.scene.image.Image;
 
+/**
+ * This class handles creation of trip objects to be sent to the client. This class also handles storage of trip objects in the database received from the client.
+ * 
+ * @author group 12
+ */
 public class ServerTripHandler {
 
-    public static void createTrip(Trip newTrip) {
-	String query = "INSERT INTO Trips "; //TODO
+	public static void createTrip(Trip newTrip) {
+		String query = "INSERT INTO Trips "; //TODO
 
-	DBManager.getInstance().executeUpdate(query);
-    }
-
-    public static List<Trip> searchTrip(String searchTitle, int category, Date timedateStart, int location, double priceMAX) {
-
-	String query = "SELECT Trips.tripID, tripTitle, description, tripPrice, imageFile FROM Trips, ImagesInTrips, Images "
-		+ "WHERE trips.tripid = imagesintrips.tripid AND images.imageID = imagesintrips.imageID AND imagesintrips.imageid IN (SELECT MIN(imageid) FROM imagesintrips GROUP BY tripid)";
-
-	if (!searchTitle.equals(null)) {
-	    query += " AND tripTitle LIKE '%" + searchTitle + "%'";
+		DBManager.getInstance().executeUpdate(query);
 	}
-
-	if (category >= 0) {
-	    query += " AND categoryID = '" + category + "'";
-	}
-
-	if (timedateStart != null) {
-	    query += " AND timeStart >= '" + timedateStart + "'";
-	}
-
-	if (location >= 0) {
-	    query += " AND locationID = '" + location + "'";
-	}
-
-	if (priceMAX >= 0) {
-	    query += " AND tripPrice <= " + priceMAX + "'";
-	}
-
-	ResultSet searchResult;
-	
-	searchResult = DBManager.getInstance().executeQuery(query);
-	
-	ArrayList<Trip> searchResultTrips = new ArrayList<>();
-	
-	try {
-	    while(searchResult.next()){
-		int id = searchResult.getInt(1);
+/**
+ * This method handles searching for trips, by building a SQL query from the given parameters.
+ * @param searchTitle used for a regex.
+ * @param category
+ * @param timedateStart
+ * @param location
+ * @param priceMAX
+ * @return list of trips matching search parameters.
+ */
+	public static List<Trip> searchTrip(String searchTitle, int category, Date timedateStart, int location, double priceMAX) {
 		
-	    
-	  
-	    searchResultTrips.add(trip);
-	    
-	    }
-	} catch (SQLException ex) {
-	    Logger.getLogger(ServerTripHandler.class.getName()).log(Level.SEVERE, null, ex);
+		//Initializes the query string.
+		String query = "SELECT Trips.tripID, tripTitle, description, tripPrice, imageFile FROM Trips, ImagesInTrips, Images "
+						+ "WHERE trips.tripid = imagesintrips.tripid AND images.imageID = imagesintrips.imageID AND imagesintrips.imageid IN (SELECT MIN(imageid) FROM imagesintrips GROUP BY tripid)";
+
+		//These if statements checks if the different parameters are used, and adds the necessary SQL code to the query string.
+		if (!searchTitle.equals(null)) {
+			query += " AND tripTitle LIKE '%" + searchTitle + "%'";
+		}
+
+		if (category >= 0) {
+			query += " AND categoryID = '" + category + "'";
+		}
+
+		if (timedateStart != null) {
+			query += " AND timeStart >= '" + timedateStart + "'";
+		}
+
+		if (location >= 0) {
+			query += " AND locationID = '" + location + "'";
+		}
+
+		if (priceMAX >= 0) {
+			query += " AND tripPrice <= " + priceMAX + "'";
+		}
+
+		//Initializes a resultset and an ArrayList for handling the creation of trips to be returned.
+		ResultSet searchResult = DBManager.getInstance().executeQuery(query);
+		ArrayList<Trip> searchResultTrips = new ArrayList<>();
+
+		try {
+			while (searchResult.next()) {
+				int id = searchResult.getInt(1);
+				String title = searchResult.getString(2);
+				String description = searchResult.getString(3);
+				double price = searchResult.getDouble(4);
+				InputStream inputStream = new ByteArrayInputStream(searchResult.getBytes(5));
+				Image image = new Image(inputStream);
+				searchResultTrips.add(new Trip(id, title, description, price, image));
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(ServerTripHandler.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		return searchResultTrips;
+
 	}
-	
-	
-	return searchResultTrips;
- 
-    }
 
-    public static void deleteTrip(Trip trip) {
-        String query = "DELETE FROM Trips WHERE tripID = " + trip.getId() + ";";
+	public static void deleteTrip(Trip trip) {
+		String query = "DELETE FROM Trips WHERE tripID = " + trip.getId() + ";";
 
-        DBManager.getInstance().executeUpdate(query);
-    }
+		DBManager.getInstance().executeUpdate(query);
+	}
 
 }
