5de2e9e332c0701788850ecfdff01e547e97d824
Nekaerth <lasse.j.lundin@gmail.com>
2017-11-17 10:23:09 +0100

    minor change

--- semesterprojekt_E17/src/client/Semesterprojekt_E17_client.java
+++ semesterprojekt_E17/src/client/Semesterprojekt_E17_client.java
@@ -7,22 +7,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Semesterprojekt_E17_client extends Application {
-    
-    @Override
-    public void start(Stage stage) throws Exception {
-	Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-	
-	Scene scene = new Scene(root);
-	
-	stage.setScene(scene);
-	stage.show();
-    }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-	launch(args);
-    }
-    
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+		Scene scene = new Scene(root);
+
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 }
