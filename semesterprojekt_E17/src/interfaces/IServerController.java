6b288ab2635a40e81b1024969df1c3ce91889415
Jungne <jungne7@gmail.com>
2017-11-29 10:18:58 +0100

    Interface fix #26 #30

--- semesterprojekt_E17/src/interfaces/IServerController.java
+++ semesterprojekt_E17/src/interfaces/IServerController.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public interface IServerController extends Remote {
 
-<<<<<<< HEAD
-	//User functions
 	public User signUp(User user, String password) throws RemoteException;
 
 	public User signIn(String username, String password) throws RemoteException;
@@ -18,7 +16,7 @@ public interface IServerController extends Remote {
 	//Trip functions    
 	public List<Trip> getAllTrips() throws RemoteException;
 
-	public List<Trip> searchTrips(String searchTitle, int category, LocalDate timedateStart, int location, double priceMAX) throws RemoteException;
+	public List<Trip> searchTrips(String searchTitle, List<Category> categories, LocalDate timedateStart, int location, double priceMAX, String tripType) throws RemoteException;
 
 	public int createTrip(Trip newTrip) throws RemoteException;
 
@@ -56,52 +54,5 @@ public interface IServerController extends Remote {
 	public List<Trip> getMyTrips(User user) throws RemoteException;
 
 	public void addActiveConversation(int tripID) throws RemoteException;
-=======
-    //User functions
-    public User signUp(User user, String password) throws RemoteException;
-    
-    public User signIn(String username, String password) throws RemoteException;
-    
-    public void signOut() throws RemoteException;
-    
-    //Trip functions    
-    public List<Trip> getAllTrips() throws RemoteException;
-    
-    public List<Trip> searchTrips(String searchTitle, List<Category> categories, LocalDate timedateStart, int location, double priceMAX, String tripType) throws RemoteException;
-    
-    public int createTrip(Trip newTrip) throws RemoteException;
-    
-    public void modifyTrip(Trip trip) throws RemoteException;
-    
-    public void deleteTrip(Trip trip) throws RemoteException;
-    
-    public void participateInTrip(Trip trip, User user) throws FullTripException, RemoteException;
-    
-    public boolean instructInTrip(Trip trip, User user) throws RemoteException;
-    
-    public void kickParticipant(Trip trip, User user) throws RemoteException;
-  
-	  public List<Category> getCategories() throws RemoteException;
 
-	  public List<Location> getLocations() throws RemoteException;
-
-	  public Trip viewTrip(int tripID) throws RemoteException;
-    
-    //Messaging functions
-    public List<Conversation> getUserConversations(User user) throws RemoteException;
-    
-    public Conversation getConversation(Conversation conversation) throws RemoteException;
-    
-    public void sendMessage(Message message) throws RemoteException;
-    
-    public void updateConversation(Conversation conversation) throws RemoteException;
-  
-    public Trip showTrip(int tripsID) throws RemoteException;
-		
-		public void registerClient(IChatClient client) throws RemoteException;
-
-		public List<User> searchUsers(String query)  throws RemoteException;
-		
-		public List<Trip> getMyTrips(User user) throws RemoteException;
->>>>>>> master
 }
